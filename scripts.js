de94b7bd57fcf1f9733174cb1dac84e0c317e302
olivianicholls <31240787+olivianicholls@users.noreply.github.com>
2017-10-06 15:13:05 +0100

    Add scripts

---
+++ scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+function createGrid(x) {
+$('.container').append("<table class='table'></table>");
+for (let i = 0; i < x; i++) {
+$('.table').append("<tr></tr>");
+}
+	for (let j = 0; j < x; j++) {
+		$('tr').append("<td></td>");
+	}
+}
+
+createGrid(16);
+function hover(){
+$('td').on('mouseenter', function() {
+  $(this).css({'background-color': 'black'});
+});
+}
+$('button').on('click', function() {
+	let popUp = prompt('How many squares?');
+	
+	if (popUp != null) {
+		$('.table').remove();
+		createGrid(popUp);
+		hover();
+	} else {
+		alert('Invalid Amount!');
+	}
+});
+});
